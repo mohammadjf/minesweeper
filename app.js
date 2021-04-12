63c3647564fe71e9736b543a51497ce886ade376
Mohammad <mohammadlngs@gmail.com>
2021-04-12 17:32:28 +0430

    added timer

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 "use strict";
 
 // Construtions
-const width = 20;
-const height = 10;
+function setGame(width, height, mode) {
+  generateBoard(width, height);
+  const squaresNumber = width * height;
+  makeRandomBombs(50, squaresNumber);
 
-generateBoard();
-makeRandomBombs(20);
-
-document.querySelectorAll(".cell").forEach((cell) => {
-  cell.addEventListener("click", (e) => {
-    openSquare(e.target);
+  document.querySelectorAll(".cell").forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      openSquare(e.target);
+    });
+    cell.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      flag(e.target);
+    });
   });
-  cell.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-    flag(e.target);
-  });
-});
 
-generateNumbers();
+  generateNumbers();
+}
 
+setGame(12, 12);
 // Functions
-function generateBoard() {
+function generateBoard(width, height) {
   document.querySelector(
     ".cell-container"
   ).style.grid = `repeat(${height}, 1fr) / repeat(${width}, 1fr)`;
-  document.querySelector("#game-over").style.cssText = `width: ${
+  document.querySelector("#message-container").style.cssText = `width: ${
     width * 30
   }px; height: ${height * 30}px`;
   for (let x = 1; x <= height; x++) {
@@ -42,26 +43,64 @@ function generateBoard() {
   }
 }
 
-function makeRandomBombs(x) {
+function makeRandomBombs(x, numEndPoint) {
   for (let i = 0; i < x; i++) {
-    let randomNum = Math.trunc(Math.random() * width * height) + 1;
+    let randomNum = Math.trunc(Math.random() * numEndPoint) + 1;
     document.querySelector(
       `.cell-container div:nth-child(${randomNum}) .square`
-    ).innerHTML = "💣";
+    ).textContent = "💣";
   }
 }
 
+function generateNumbers() {
+  document.querySelectorAll(".cell").forEach((cell) => {
+    const square = cell.children[1];
+    if (square.textContent === "💣") return true;
+    let number = 0;
+    const dividedID = cell.id.split("-");
+    const x = +dividedID[1];
+    const y = +dividedID[2];
+    const upSquare = document.querySelector(`#cell-${x - 1}-${y} .square`);
+    const downSquare = document.querySelector(`#cell-${x + 1}-${y} .square`);
+    const rightSquare = document.querySelector(`#cell-${x}-${y + 1} .square`);
+    const leftSquare = document.querySelector(`#cell-${x}-${y - 1} .square`);
+    const upRightSquare = document.querySelector(
+      `#cell-${x - 1}-${y + 1} .square`
+    );
+    const upLeftSquare = document.querySelector(
+      `#cell-${x - 1}-${y - 1} .square`
+    );
+    const downRightSquare = document.querySelector(
+      `#cell-${x + 1}-${y + 1} .square`
+    );
+    const downLeftSquare = document.querySelector(
+      `#cell-${x + 1}-${y - 1} .square`
+    );
+    if (upSquare?.textContent === "💣") number++;
+    if (downSquare?.textContent === "💣") number++;
+    if (rightSquare?.textContent === "💣") number++;
+    if (leftSquare?.textContent === "💣") number++;
+    if (upRightSquare?.textContent === "💣") number++;
+    if (upLeftSquare?.textContent === "💣") number++;
+    if (downRightSquare?.textContent === "💣") number++;
+    if (downLeftSquare?.textContent === "💣") number++;
+    square.textContent = number === 0 ? "" : number;
+  });
+}
+
 function openSquare(cell) {
+  if (!stopWatch) startTimer();
   const sameCellFlag = document.querySelector(`#${cell?.id} .flag`);
   const square = cell?.children[1];
   if (square?.classList.contains("hidden")) {
     square.classList.remove("hidden");
     sameCellFlag?.classList.contains("hidden") ||
       sameCellFlag?.classList.add("hidden");
-    if (square.innerHTML == 0) {
+    if (square.textContent == 0) {
       openAroundSquares(cell);
-    } else if (square.innerHTML === "💣") explosion(cell);
+    } else if (square.textContent === "💣") explosion(cell);
   }
+  checkWin();
 }
 
 function flag(node) {
@@ -70,6 +109,7 @@ function flag(node) {
       ? node.children[0].classList.remove("hidden")
       : node.classList.add("hidden");
   }
+  checkWin();
 }
 
 function openAroundSquares(cell) {
@@ -94,55 +134,62 @@ function openAroundSquares(cell) {
   openSquare(downLeftSquare);
 }
 
-function generateNumbers() {
-  document.querySelectorAll(".cell").forEach((cell) => {
-    const square = cell.children[1];
-    if (square.innerHTML === "💣") return true;
-    let number = 0;
-    const dividedID = cell.id.split("-");
-    const x = +dividedID[1];
-    const y = +dividedID[2];
-    const upSquare = document.querySelector(`#cell-${x - 1}-${y} .square`);
-    const downSquare = document.querySelector(`#cell-${x + 1}-${y} .square`);
-    const rightSquare = document.querySelector(`#cell-${x}-${y + 1} .square`);
-    const leftSquare = document.querySelector(`#cell-${x}-${y - 1} .square`);
-    const upRightSquare = document.querySelector(
-      `#cell-${x - 1}-${y + 1} .square`
-    );
-    const upLeftSquare = document.querySelector(
-      `#cell-${x - 1}-${y - 1} .square`
-    );
-    const downRightSquare = document.querySelector(
-      `#cell-${x + 1}-${y + 1} .square`
-    );
-    const downLeftSquare = document.querySelector(
-      `#cell-${x + 1}-${y - 1} .square`
-    );
-    if (upSquare?.innerHTML === "💣") number++;
-    if (downSquare?.innerHTML === "💣") number++;
-    if (rightSquare?.innerHTML === "💣") number++;
-    if (leftSquare?.innerHTML === "💣") number++;
-    if (upRightSquare?.innerHTML === "💣") number++;
-    if (upLeftSquare?.innerHTML === "💣") number++;
-    if (downRightSquare?.innerHTML === "💣") number++;
-    if (downLeftSquare?.innerHTML === "💣") number++;
-    square.innerHTML = number === 0 ? "" : number;
-  });
-}
-
-function showGameOver() {
-  document.querySelector("#game-over").style.opacity = "100";
-  document.querySelector("#game-over").classList.remove("hidden");
+function showmessage(message) {
+  const html = `
+  <h2 id="message">${message}</h2>
+  <p id="score"> Your Time : ${getTime()}</p>
+  `;
+  document
+    .querySelector("#message-container")
+    .insertAdjacentHTML("afterbegin", html);
+  document.querySelector("#message-container").style.opacity = "100";
+  document.querySelector("#message-container").classList.remove("hidden");
 }
 
 function explosion(cell) {
-  cell.children[1].innerHTML = "💥";
+  cell.children[1].textContent = "💥";
   document.querySelectorAll(".cell").forEach((cell) => {
     if (
-      cell.children[1].innerHTML === "💣" &&
+      cell.children[1].textContent === "💣" &&
       cell.children[0].classList.contains("hidden")
     )
       cell.children[1].classList.remove("hidden");
   });
-  setTimeout(showGameOver);
+  showmessage("You Lost 😵");
+  clearInterval(stopWatch);
+}
+
+function showCongrats() {}
+
+function checkWin() {
+  let won = true;
+  document.querySelectorAll(".cell").forEach((cell) => {
+    if (
+      cell.children[1].textContent === "💣" &&
+      cell.children[1].classList.contains("covered")
+    )
+      won = false;
+  });
+  won && showCongrats();
+}
+
+let stopWatch;
+function startTimer() {
+  function tick() {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    document.querySelector("#stop-watch").textContent = `${min}:${sec}`;
+
+    time++;
+  }
+
+  let time = 0;
+
+  tick();
+  stopWatch = setInterval(tick, 1000);
+}
+
+function getTime() {
+  return document.querySelector("#stop-watch").textContent;
 }
