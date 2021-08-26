889a37cdad0d75b4bc55a9f5de9c7acf49aae8c2
Mohammad Jafari <mohammadlngs@gmail.com>
2021-08-26 10:47:15 +0430

    initial styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function Cell({ value }) {
+  const [isHidden, setIsHidden] = useState(true);
+
+  return isHidden ? (
+    <div
+      className="cell hidden"
+      onClick={() => {
+        setIsHidden(false);
+      }}
+    ></div>
+  ) : (
+    <div className="cell">{value}</div>
+  );
+}
+
+function Board({ width, height }) {
+  return (
+    <div className="board">
+      <div className="row">
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+      </div>
+      <div className="row">
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+      </div>
+      <div className="row">
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+      </div>
+      <div className="row">
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+        <Cell value="2"></Cell>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <Board></Board>
     </div>
   );
 }
