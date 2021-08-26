0b1fc58a6ea97ea5aae7b8ad05f806afca9084a1
Mohammad Jafari <mohammadlngs@gmail.com>
2021-08-26 12:19:48 +0430

    not working :[

--- src/App.js
+++ src/App.js
@@ -3,20 +3,59 @@ import { useEffect, useState } from "react";
 
 function Cell({ value }) {
   const [isHidden, setIsHidden] = useState(true);
+  const [flag, setFlag] = useState(null);
 
   return isHidden ? (
     <div
       className="cell hidden"
       onClick={() => {
-        setIsHidden(false);
+        setIsHidden(flag === "🚩");
       }}
-    ></div>
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setFlag(flag === null ? "🚩" : null);
+      }}
+    >
+      {flag}
+    </div>
   ) : (
-    <div className="cell">{value}</div>
+    <div
+      className="cell"
+      onContextMenu={(e) => {
+        e.preventDefault();
+      }}
+    >
+      {value}
+    </div>
   );
 }
 
 function Board({ width, height }) {
+  const [cells, setCells] = useState(new Array(width * height));
+
+  function bombsAndNumbersGenerator(height, width) {
+    let _cells = [];
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        _cells.push({ x: i, y: j, value: null });
+      }
+    }
+    // _cells = populateCellsWithBombs(8, _cells);
+
+    setCells(_cells);
+  }
+
+  function populateCellsWithBombs(totalBombnumber, cellsArray) {
+    for (let i = 0; i < totalBombnumber; i++) {
+      let randomNum = Math.trunc(Math.random() * (cellsArray.length - 1));
+      cellsArray[randomNum].value = "💣";
+    }
+    return cellsArray;
+  }
+  useEffect(() => {
+    bombsAndNumbersGenerator(width, height);
+  }, []);
+
   return (
     <div className="board">
       <div className="row">
@@ -58,7 +97,7 @@ function Board({ width, height }) {
 function App() {
   return (
     <div className="App">
-      <Board></Board>
+      <Board width={15} height={15}></Board>
     </div>
   );
 }
